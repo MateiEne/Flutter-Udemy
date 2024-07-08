ec466500ce502f28718df091c03c00d3057d02cc
Ene Matei <ene.matei21@gmail.com>
2024-07-08 18:27:58 +0300

    added shopping list app project

---
+++ ShoppingListApp/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_app/screens/home.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
