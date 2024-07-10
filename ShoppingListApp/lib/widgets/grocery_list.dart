3671f2518691e0f4562aeb25d89a1aa4e9a9c988
Ene Matei <ene.matei21@gmail.com>
2024-07-10 15:31:32 +0300

    added dissmisible widget to remove items from list

--- ShoppingListApp/lib/widgets/grocery_list.dart
+++ ShoppingListApp/lib/widgets/grocery_list.dart
@@ -29,8 +29,46 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text("No items added yet!"),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        itemCount: _groceryItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Dismissible(
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Theme.of(context).colorScheme.error,
+            ),
+            onDismissed: (direction) {
+              _removeItem(_groceryItems[index]);
+            },
+            key: ValueKey(_groceryItems[index].id),
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.color,
+              ),
+              trailing: Text("${_groceryItems[index].quantity}"),
+            ),
+          );
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Groceries"),
@@ -43,25 +81,7 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: _groceryItems.isNotEmpty
-          ? ListView.builder(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              itemCount: _groceryItems.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(_groceryItems[index].name),
-                  leading: Container(
-                    width: 24,
-                    height: 24,
-                    color: _groceryItems[index].category.color,
-                  ),
-                  trailing: Text("${_groceryItems[index].quantity}"),
-                );
-              },
-            )
-          : const Center(
-              child: Text("No items added yet!"),
-            ),
+      body: content,
     );
   }
 }
