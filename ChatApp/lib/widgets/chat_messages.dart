25d1b74455535a0f570bba66510df4651670958c
Ene Matei <ene.matei21@gmail.com>
2024-08-27 15:09:53 +0300

    can upload messages on firebase database and display them on screen

---
+++ ChatApp/lib/widgets/chat_messages.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'createdAt',
+            descending: false,
+          )
+          .snapshots(),
+      builder: (BuildContext ctx, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found.'),
+          );
+        }
+
+        if (chatSnapshots.hasError) {
+          return const Center(
+            child: Text('Something went wrong...'),
+          );
+        }
+
+        final loadedMessages = chatSnapshots.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Text(loadedMessages[index].get('text'));
+          },
+        );
+      },
+    );
+  }
+}
